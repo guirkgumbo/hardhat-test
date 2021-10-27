23cb6bcec9346ef94811c05e17addecf0e7b05e3
guirk <rmcguirk109@gmail.com>
2021-10-27 17:47:45 -0400

    setting up for deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,30 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+// scripts/deploy.js
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const Box = await ethers.getContractFactory("Box");
+  console.log("Deploying Box...");
+  const box = await Box.deploy();
+  await box.deployed();
+  console.log("Box deployed to:", box.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  // eslint-disable-next-line no-process-exit
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    // eslint-disable-next-line no-process-exit
+    process.exit(1);
+  });
